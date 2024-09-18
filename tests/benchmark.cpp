6da242621b3c851d82197754c4107e8d1cc0414b
Yitao Huang <yitao@umich.edu>
2024-09-17 21:33:30 -0700

    code style updates

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -2,42 +2,37 @@
 #include <chrono>
 #include <iostream>
 
-struct ControlClass {
-    std::uint32_t value;
+struct ControlClass 
+{
+    uint32_t value;
     std::string metadata;
-
-    uint32_t& getHotData() noexcept
-    {
-        return value;
-    }
-
-    uint32_t const& getHotData() const noexcept
-    {
-        return value;
-    }
 };
 
-struct TestClass : public CacheEntry<uint32_t, std::string> {};
+struct TestClass 
+    : public CacheEntryExternal<TestClass, std::string> 
+{
+    uint32_t value;
+};
 
 template <class Data>
 auto generateData() {
-    srand(20240916);
+    srand(108289);
     std::vector<Data> data(10000000);
     for (Data& d : data) 
     {
-        d.getHotData() = rand();
+        d.value = rand();
     }
     return data;
 }
 
 template <class Data>
 void benchmark(const char* const name, const Data& data, const bool print) {
-    std::uint32_t running = 0U;
+    uint32_t running = 0;
     const auto before = std::chrono::system_clock::now();
 
     for (const auto& d : data)
     {
-        running += d.getHotData();
+        running += d.value;
     }
 
     const auto after = std::chrono::system_clock::now();
